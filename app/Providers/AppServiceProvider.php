37d2447309d2e54a77510a206696673ea7c46f33
sameer <sameer.smartitventures@gmail.com>
2022-09-04 01:53:50 +0530

    easy fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 
-    /*\URL::forceRootUrl(\Config::get('app.url'));    
+    \URL::forceRootUrl(\Config::get('app.url'));    
     if (str_contains(\Config::get('app.url'), 'https://')) {
         \URL::forceScheme('https');
-    }*/
+    }
 
 
     }
